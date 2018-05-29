1b35181521ebf7fe54051e66f67c4595cdd38f76
good5229 <good5229@naver.com>
2018-05-29 15:46:10 +0900

    feat(board): add update function

--- board/blog/urls.py
+++ board/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path
 
-from board.blog.views import PostList, PostDetail, PostCreate
+from board.blog.views import PostList, PostDetail, PostCreate, update
 
 app_name = 'blog'
 
 urlpatterns = [
     re_path(r'^$', PostList.as_view(), name='home'),
+    re_path(r'^create/$', PostCreate.as_view(), name='create'),
     re_path(r'(?P<pk>\d+)/$', PostDetail.as_view(), name='detail'),
-    re_path(r'^create/$', PostCreate.as_view(), name='create')
+    re_path(r'(?P<pk>\d+)/update$', update, name='update')
 ]
--- board/blog/views.py
+++ board/blog/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, CreateView
 
+from board.blog.forms import PostForm
 from .models import Post
 
 
@@ -16,4 +18,19 @@ class PostDetail(DetailView):
 class PostCreate(CreateView):
     model = Post
     template_name = 'create.html'
-    fields = ['category', 'title', 'content']
+    form_class = PostForm
+
+
+def update(request, pk):
+    if request.method == "POST":
+        # 수정 저장
+        post = Post.objects.get(pk=pk)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect(post)
+    else:
+        # 수정 입력
+        post = Post.objects.get(pk=pk)
+        form = PostForm(instance=post)
+        return render(request, 'create.html', {'object': post, 'form': form})
