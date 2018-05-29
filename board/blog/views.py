45e217d59d06a020c9517e4241c1e416db64b4c2
good5229 <good5229@naver.com>
2018-05-29 11:13:40 +0900

    first commit

---
+++ board/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def home(request):
+    template_name = 'home.html'
+    posts = Post.objects.all()
+    context_data = {'object_list': posts}
+    return render(request, template_name, context_data)
+
+
+def detail(request, pk):
+    template_name = 'detail.html'
+    post = get_object_or_404(Post, id=pk)
+    context_data = {'object': post}
+    return render(request, template_name, context_data)
